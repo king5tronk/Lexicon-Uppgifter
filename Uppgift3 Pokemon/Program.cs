77c2a5b4ac2b01c9707b58db53a017877f7dfe52
Tompa123 <mrguy_93@hotmail.com>
2025-05-28 17:17:03 +0200

    Added Uppgift3 Folder

---
+++ Uppgift3 Pokemon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Uppgift3_Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
